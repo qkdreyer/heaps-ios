30aab6ec225f809da3f9600c24fe932ea9187213
qkdreyer <quentin.dreyer@rgsystem.com>
2020-03-12 08:04:06 +0100

    build: remove _fmt_dxt_decode patch

--- app-patch.sh
+++ app-patch.sh
@@ -9,8 +9,3 @@ echo "Patching Hashlink SDL2 Header Path"
 pushd hashlink
 curl https://gist.githubusercontent.com/qkdreyer/1a22b10fe4064554f724bf5a400fe470/raw/8524ef768e79c1668eb5cea8fbf7216ca816f5b9/gistfile1.txt | git apply
 popd
-
-echo "Patching Linker Heaps _fmt_dxt_decode Missing Symbol"
-pushd heaps
-curl https://gist.githubusercontent.com/qkdreyer/54245f242ce69d61bd4816e135dcb930/raw/e6392463adfa438110d06a0d65c289009c73400a/gistfile1.txt | git apply
-popd
