a33be67ffa692c0549d3d83686c591f0e2304b0c
qkdreyer <quentin.dreyer@rgsystem.com>
2020-03-12 01:01:13 +0100

    style: use single quotas in scripts

--- app-post-clone.sh
+++ app-post-clone.sh
@@ -1,28 +1,31 @@
 #!/bin/sh
 pushd deps
 
-echo "Installing Haxe"
+echo 'Installing Haxe'
 brew install haxe
-echo "Using haxe version "`haxe -version`
-echo "Setting up haxelib"
-haxelib setup "/usr/local/lib/haxe/lib"
-echo "Using haxelib version "`haxelib version`
+echo 'Using haxe version '`haxe -version`
+echo 'Setting up haxelib'
+haxelib setup '/usr/local/lib/haxe/lib'
+echo 'Using haxelib version '`haxelib version`
 
 pushd hashlink
 
-echo "Fetching Hashlink Dependencies"
+echo 'Fetching Hashlink Dependencies'
 brew install libpng jpeg-turbo libvorbis sdl2 mbedtls openal-soft libuv
 
-echo "Installing Hashlink"
+echo 'Installing Hashlink'
 make all
 make install
 popd
 
-echo "Installing Haxelib Dependencies"
+echo 'Installing Haxelib Dependencies'
 haxelib install hlsdl
 haxelib install hlopenal
 haxelib install hashlink
 haxelib install format
 
-echo "Fetching Dependencies"
+echo 'Fetching Dependencies'
 git submodule update --remote --depth 1 --init --recursive
+
+
+echo '.gitignore\nbuild' > deps/SDL/.gitignore
--- app-pre-build.sh
+++ app-pre-build.sh
@@ -5,13 +5,13 @@ HXML=$PWD/.hxml
 cd `dirname $(realpath $HXML)`
 pwd
 
-echo "Installing libraries..."
+echo 'Installing libraries...'
 haxelib install $HXML --always
 
-echo "Generating native code..."
+echo 'Generating native code...'
 haxe -hl $OUT/main.c $HXML || exit 1
 
-echo "Packing content..."
+echo 'Packing content...'
 haxe -L heaps -main hxd.fmt.pak.Build -hl $OUT/pak.hl || exit 2
 hl $OUT/pak.hl -out $OUT/res && rm $OUT/pak.hl || exit 3
 sleep 1
